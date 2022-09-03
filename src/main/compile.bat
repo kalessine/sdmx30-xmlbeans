84a567890da42c38c8acb9e4533f539817b07058
James Gardner <jsg@internode.on.net>
2022-09-03 22:35:35 +0800

    initial

---
+++ src/main/compile.bat
@@ -0,0 +1,2 @@
+set PATH=%PATH%;C:\Progra~1\Java\jdk-13.0.2\bin
+C:\xmlbeans-5.1.0\bin\scomp -out sdmx30.jar -partialMethods ALL,-SET_ARRAY .\schema\*.xsd .\xml\xmlconfig.xsdconfig .\java\mypackage\Foo.java .\java\mypackage\FooHandler.java
---
+++ src/main/java/mypackage/Foo.java
@@ -0,0 +1,14 @@
+package mypackage;
+
+public interface Foo {
+    String getPackage();
+    String getURNClass();
+    
+    String getAgencyID();
+
+    String getParentID();
+
+    String getVersion();
+
+    String getID();
+}
